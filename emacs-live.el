f89b7cf5562fd702bc68a93f571f782d59e80087
Simon Jagoe <sjagoe@enthought.com>
2013-05-18 18:58:39 +0100

    Added enaml-pack

--- emacs-live.el
+++ emacs-live.el
@@ -1,6 +1,7 @@
 (live-add-packs '("~/.live-packs/cython-pack"))
 (live-add-packs '("~/.live-packs/desktop-pack"))
 (live-add-packs '("~/.live-packs/disorganized-pack"))
+(live-add-packs '("~/.live-packs/enaml-pack"))
 (live-add-packs '("~/.live-packs/orgmode-pack"))
 (live-add-packs '("~/.live-packs/modeline-pack"))
 (live-add-packs '("~/.live-packs/whitespace-pack"))
