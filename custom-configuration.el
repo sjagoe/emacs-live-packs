b364124eeb033532e02954d17b5a193752b8a697
Simon Jagoe <sjagoe@enthought.com>
2013-11-06 17:36:46 +0000

    Added base of custom configuration

---
+++ custom-configuration.el
@@ -0,0 +1,30 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-undo-tree-mode t)
+ '(magit-default-tracking-name-function (quote magit-default-tracking-name-branch-only))
+ '(sclang-auto-scroll-post-buffer t)
+ '(sclang-eval-line-forward nil)
+ '(undo-tree-visualizer-lazy-drawing 1000000)
+ '(whitespace-action (quote (auto-cleanup)))
+ '(whitespace-line-column 79))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(diff-added ((t (:foreground "Green"))))
+ '(diff-removed ((t (:foreground "Red"))))
+ '(ediff-even-diff-A ((((class color) (background dark)) (:background "dark green"))))
+ '(ediff-even-diff-B ((((class color) (background dark)) (:background "dark red"))))
+ '(ediff-odd-diff-A ((((class color) (background dark)) (:background "dark green"))))
+ '(ediff-odd-diff-B ((((class color) (background dark)) (:background "dark red"))))
+ '(eval-sexp-fu-flash ((((class color) (background dark)) (:background "grey15" :foreground "DeepPink3"))))
+ '(flycheck-error-face ((t (:background "gray38"))))
+ '(flycheck-warning-face ((t (:background "gray25"))))
+ '(mumamo-background-chunk-major ((((class color) (background dark)) (:background "black"))))
+ '(mumamo-background-chunk-submode1 ((((class color) (background dark)) (:background "black"))))
+ '(org-done ((t (:foreground "PaleGreen" :weight normal :strike-through t))))
+ '(org-headline-done ((((class color) (min-colors 16) (background dark)) (:foreground "LightSalmon" :strike-through t)))))
